4c53c5fb55fe9b34a9eed36d4b754b886f508f8a
Kiranwghmare <waghmarek749@gmail.com>
2024-11-05 19:40:01 +0530

    Task

---
+++ Task.java
@@ -0,0 +1,24 @@
+class Task{
+
+    private int id;
+    private String title;
+    private boolean isCompleted;
+
+
+    public Task(){
+
+    }
+
+    public Task(int id, String title){
+
+        this.id= id;
+        this.title=title;
+        this.isCompleted=false;
+
+    }
+
+    //getters and Setters
+    
+
+
+}
